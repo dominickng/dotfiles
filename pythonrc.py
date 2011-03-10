e6e6cb11859d47d4c3a24eeaa7e08cfea14efb82
Dominick Ng <dominick8n@gmail.com>
2011-03-11 09:21:32 +1100

    * better code style for pythonrc

--- pythonrc.py
+++ pythonrc.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 
+import atexit
 import os
 import readline
 import rlcompleter
-import readline
-import atexit
+import sys
 
 defaultCompleter = rlcompleter.Completer(locals())
 
-historyPath = os.path.expanduser("~/.pyhistory")
-historySize = 1000
+histfile = os.path.expanduser("~/.pyhistory")
+histsize = 1000
 
 def myCompleter(text, state):
-  if text.strip() == "" and state == 0:
-    return text + "\t"
-  else:
-    return defaultCompleter.complete(text, state)
+    if text.strip() == "" and state == 0:
+        return text + "\t"
+    else:
+        return defaultCompleter.complete(text, state)
 
-def save_history(historyPath=historyPath):
-  import readline
-  readline.set_history_length(historySize)
-  readline.write_history_file(historyPath)
+def save_history(histfile=histfile):
+    import readline
+    readline.set_history_length(histsize)
+    readline.write_history_file(histfile)
 
 readline.set_completer(myCompleter)
 readline.parse_and_bind("tab: complete")
 
-if os.path.exists(historyPath):
-  readline.read_history_file(historyPath)
+if os.path.exists(histfile):
+    readline.read_history_file(histfile)
 
 atexit.register(save_history)
 
 del rlcompleter, readline, atexit
-
