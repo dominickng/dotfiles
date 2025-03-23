324aa1bf91192f5a9f3ab95cd90ce14f2ef99a35
Dominick Ng <dominickng@users.noreply.github.com>
2025-03-23 23:11:16 +1100

    Add macro to remember last place in file.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -89,6 +89,25 @@ vim.opt.wildignore:append({ "*.zip", "*.tar.gz", "*.tar.bz2", "*.rar", "*.tar.xz
 vim.opt.wildignore:append({ "*.git", "*.svn", "*.hg", "*.bzr", "*.vagrant", "*.DS_Store", "tmp", })
 vim.opt.wildignore:append({ "*.jpg", "*.jpeg", "*.png", "*.gif", "*.bmp", "*.tiff", "*.ico", "*.svg", "*.webp", "*.pdf" })
 
+-- When editing a file, always jump to the last known cursor position.
+-- Don't do it when the position is invalid, when inside an event handler
+-- (happens when dropping a file on gvim) and for a commit message (it's
+-- likely a different one than last time).
+local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
+vim.api.nvim_clear_autocmds({ group = lastplace })
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = lastplace,
+  desc = "remember last cursor place",
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
+
 -- vim.cmd([[
 -- " Chops a filename down to max_length characters, replacing
 -- " the middle with ...
